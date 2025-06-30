78a516c5ae9288e7cbfc15ab210111ce1533bafd
0xskaper <es.rajatyadav@gmail.com>
2025-06-30 07:41:16 -0400

    feat: postgres connection

---
+++ backend/internal/database/postgres.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresConnection(databaseURL string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+
+	return db, nil
+}
